1cfcc2ca7b0edee327a37b9aeddeec8dce76f0bd
Grominola <andrea.garcia192@gmail.com>
2025-11-24 16:09:57 +0100

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,2 @@
-self.addEventListener('install', function(event) {
-  console.log('Service Worker instalado');
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
-});
+self.addEventListener('install', function(event){ console.log('Service Worker instalado'); });
+self.addEventListener('fetch', function(event){ event.respondWith(fetch(event.request)); });
