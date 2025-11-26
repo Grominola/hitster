be4e77e0b4d0f07bd0a8cb9cffb64e3f2ef391a1
villavalira <andrea.garcia192@gmail.com>
2025-11-26 11:38:12 +0100

    sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'hitster-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/fondo.png',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Instalación
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activación
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+});
+
+// Interceptar peticiones
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
